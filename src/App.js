552df9b7a37cc79012b3b08df01242025f9128f1
GodfredAsa <goldenheroe1@yahoo.com>
2022-05-25 08:19:17 +0000

    implementing header cart icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from 'react';
-import Header from './components/Layout/Header';
+import Header from './components/Layout/Header/Header';
 
 const  App = () => {
   return (
     <Fragment>
+
       <Header/>
+
     </Fragment>
   );
 }
