595c4f5fc9c7a44a5568f59c33bd65568fbc9cdf
GodfredAsa <goldenheroe1@yahoo.com>
2022-05-25 05:05:01 +0000

    initial setup commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h2 className="text-warning">Let's get started!</h2>
+      <i className="bi bi-activity"></i>
+      <i className="bi bi-trash h1"></i>
+      <button className="btn btn-sm btn-secondary">Send</button>
+    </div>
+  );
+}
+
+export default App;
